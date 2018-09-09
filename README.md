511cd454585117fb510aa3b40aca0f38e2f8c4f8
shubham <ishubhamjain@gmail.com>
2018-09-09 11:10:33 +0530

    adding base project

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # MavenJenkins
+MavenJenkins
+
+Steps :
+
+1. Download the repo.
+2. Install maven in your machine
+3. configure IDE with all required plug-ins like cucumber, maven plug-ins
---
+++ target/site/cucumber-pretty/report.js
@@ -0,0 +1,107 @@
+$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("A.LogIn_Test.feature");
+formatter.feature({
+  "comments": [
+    {
+      "line": 1,
+      "value": "#Author: shubham.jain@cuelogic.co.in"
+    },
+    {
+      "line": 2,
+      "value": "#Keywords Summary :"
+    },
+    {
+      "line": 3,
+      "value": "#Feature: List of scenarios."
+    },
+    {
+      "line": 4,
+      "value": "#Scenario: Business rule through list of steps with arguments."
+    },
+    {
+      "line": 5,
+      "value": "#Given: Some precondition step"
+    },
+    {
+      "line": 6,
+      "value": "#When: Some key actions"
+    },
+    {
+      "line": 7,
+      "value": "#Then: To observe outcomes or validation"
+    },
+    {
+      "line": 8,
+      "value": "#And,But: To enumerate more Given,When,Then steps"
+    },
+    {
+      "line": 9,
+      "value": "#Scenario Outline: List of steps for data-driven as an Examples and \u003cplaceholder\u003e"
+    },
+    {
+      "line": 10,
+      "value": "#Examples: Container for s table"
+    },
+    {
+      "line": 11,
+      "value": "#Background: List of steps run before each of the scenarios"
+    },
+    {
+      "line": 12,
+      "value": "#\"\"\" (Doc Strings)"
+    },
+    {
+      "line": 13,
+      "value": "#| (Data Tables)"
+    },
+    {
+      "line": 14,
+      "value": "#@ (Tags/Labels):To group Scenarios"
+    },
+    {
+      "line": 15,
+      "value": "#\u003c\u003e (placeholder)"
+    },
+    {
+      "line": 16,
+      "value": "#\"\""
+    },
+    {
+      "line": 17,
+      "value": "## (Comments)"
+    },
+    {
+      "line": 19,
+      "value": "#Sample Feature Definition Template"
+    }
+  ],
+  "line": 21,
+  "name": "Testing Framework Action",
+  "description": "",
+  "id": "testing-framework-action",
+  "keyword": "Feature"
+});
+formatter.scenario({
+  "line": 23,
+  "name": "Testing my Framework cases",
+  "description": "",
+  "id": "testing-framework-action;testing-my-framework-cases",
+  "type": "scenario",
+  "keyword": "Scenario"
+});
+formatter.step({
+  "line": 24,
+  "name": "Test1",
+  "keyword": "Given "
+});
+formatter.match({
+  "location": "Test_Steps4.Test1()"
+});
+formatter.result({
+  "duration": 24404179457,
+  "status": "passed"
+});
+formatter.after({
+  "duration": 10082899365,
+  "status": "passed"
+});
+});
